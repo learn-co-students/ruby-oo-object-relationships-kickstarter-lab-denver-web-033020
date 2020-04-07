bd3e4a4bbd2a0154ad54db8ee8a2c819098fbf8e
wolf-dominion <hannah_drury@yahoo.com>
2020-04-07 12:43:18 -0600

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+class Backer
+    attr_reader :name
+    
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        backed = ProjectBacker.all.select {|project| project.backer == self}
+        backed.collect {|n| n.project}
+    end
+
+end
